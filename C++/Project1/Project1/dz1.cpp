c181f5ba2c3f415781cfa7246556993819df3ea0
Lestt12345 <promtestexp@gmail.com>
2024-04-21 14:26:17 +0300

    ыфв

---
+++ C++/Project1/Project1/dz1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+//int main() {
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//	int R1=5;
+//	int R2=3;
+//	int R3=6;
+//	float R0=1.0/(1.0/R1+1.0/R2+1.0/R3);
+//	cout << "R1=" << R1 << ", R2=" << R2 << ", R3=" << R3 << " R0 = " << R0;
+//}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+}
