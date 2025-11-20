bdef2cab405b198e9ee3328f49f51e26308b5133
Lestt12345 <promtestexp@gmail.com>
2025-11-20 21:16:48 +0200

    asd

--- CSharp/WebApplication2/Pages/Task1.cshtml.cs
+++ CSharp/WebApplication2/Pages/Task1.cshtml.cs
@@ -1,16 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication2.Pages
 {
     public class Task1Model : PageModel
     {
-        public string Message { get; set; }
+        [BindProperty]
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
 
-        public void OnGet(string message)
+        [BindProperty]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [BindProperty]
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [BindProperty]
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [BindProperty]
+        [Range(18, 100)]
+        public int Age { get; set; }
+
+        public IActionResult OnPost()
         {
-            Message = message;
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            TempData["seccess"] = "Registration complete!";
+            return RedirectToPage("/Seccess");
         }
-
     }
 }
