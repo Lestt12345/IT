ebacd4943bb263606db4b13427aaa949c67527e7
Lestt12345 <promtestexp@gmail.com>
2025-11-20 21:18:24 +0200

    asd

--- CSharp/WebApplication2/Pages/Task3.cshtml.cs
+++ CSharp/WebApplication2/Pages/Task3.cshtml.cs
@@ -10,7 +10,7 @@ namespace WebApplication2.Pages
         [EmailAddress]
         public string Email { get; set; }
 
-        public void OnGet()
+        public void OnGet() //asd
         {
         }
 
